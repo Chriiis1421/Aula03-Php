9d37c7498fdd470c837eb13d4622773f70fe224d
Chriiis1421 <106244596+Chriiis1421@users.noreply.github.com>
2023-08-23 16:46:19 -0300

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//    return view('welcome');
+// });
+
+Route::get('/alunos', function() {
+
+    $dados = array(
+        "Carlos Eduardo",
+        "Maria Cláudia",
+        "João Pedro"
+    );
+
+    $total = sizeof($dados, 0);
+
+    $alunos = "<ul>";
+
+    if($total <= count($dados)) {
+        $cont = 0;
+        foreach($dados as $nome) {
+            $cont++;  
+            $alunos .= "<li>$cont - $nome</li>";
+            $cont--;  
+            $cont++;
+            if($cont >= $total) break;
+        }
+    }
+    else {
+        $alunos = $alunos."<li>Tamanho Máximo = ".count($dados)."</li>";
+    }
+
+    $alunos .= "</ul>";
+
+    return $alunos;
+});
+
+
